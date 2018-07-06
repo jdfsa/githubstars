4ee90959f2bcb0647a85ac93bad76e538a379216
jacques.sa@outlook.com <jacques.sa@outlook.com>
2018-07-06 14:55:15 -0300

    helper for generating configuration mock

---
+++ Api/Api.Test/Helper/ConfigurationHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Moq;
+
+namespace Api.Test.Helper
+{
+    /// <summary>
+    /// Configuration mock generator
+    /// </summary>
+    public static class ConfigurationHelper
+    {
+        /// <summary>
+        /// Returns a mocked instance of IConfiguration
+        /// </summary>
+        /// <returns></returns>
+        public static IConfiguration GetConfigurationMock()
+        {
+            var configuration = new Mock<IConfiguration>();
+            configuration.Setup(g => g["GitHubData:ClientId"]).Returns("_client_id_test");
+            configuration.Setup(g => g["GitHubData:ClientSecret"]).Returns("_client_secret_test");
+            configuration.Setup(g => g["GitHubData:Scope"]).Returns("_scope:t1,scope:t2");
+            configuration.Setup(g => g["GitHubEndpoints:Authorize"]).Returns("http://url-test.com/authorize");
+            configuration.Setup(g => g["GitHubEndpoints:AccessToken"]).Returns("http://url-test.com/access_token");
+            configuration.Setup(g => g["GitHubEndpoints:GraphQL"]).Returns("http://url-test.com/graphql");
+            return configuration.Object;
+        }
+    }
+}
