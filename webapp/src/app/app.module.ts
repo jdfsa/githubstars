8ad43a0fd57f807893c4ca0380bdff38ac712a74
jacques.sa@outlook.com <jacques.sa@outlook.com>
2018-07-06 01:46:55 -0300

    search component logic

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
@@ -24,6 +24,7 @@ import { RepositoryService } from './services/repository.service';
     SearchContentComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
--- webapp/src/app/components/search/search.component.ts
+++ webapp/src/app/components/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -7,13 +8,27 @@ import { UserService } from '../../services/user.service';
 })
 export class SearchComponent implements OnInit {
   data: any;
+  search: string;
 
-  constructor(private userService: UserService) { }
+  constructor(
+    private router: Router,
+    private active: ActivatedRoute,
+    private userService: UserService) { }
 
   ngOnInit() {
     this.userService.getUserData().subscribe(data => {
       this.data = data.data;
     });
+
+    this.active.queryParams.subscribe(params => {
+      if (params['search']) {
+        this.search = params['search'];
+      }
+    });
+  }
+
+  public doSearch() {
+    this.router.navigateByUrl('/list?search=' + this.search);
   }
 
 }
