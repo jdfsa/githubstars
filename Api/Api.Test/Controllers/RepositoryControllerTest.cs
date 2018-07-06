ffd9c4b8286a5d14eeed01bafa0c310d1df59454
jacques.sa@outlook.com <jacques.sa@outlook.com>
2018-07-06 15:02:24 -0300

    repository controller with tests

---
+++ Api/Api.Test/Controllers/RepositoryControllerTest.cs
@@ -0,0 +1,61 @@
+﻿using Api.Controllers;
+using Api.Model;
+using Api.Test.Fakes;
+using Api.Test.Helper;
+using GraphQL.Common.Response;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Api.Test.Controllers
+{
+    public class RepositoryControllerTest
+    {
+        RepositoryController controller;
+
+        public RepositoryControllerTest()
+        {
+            var configuration = ConfigurationHelper.GetConfigurationMock();
+            var service = new GraphQLServiceFake("http://fakeendpoint.com/fake")
+            {
+                Response = new GraphQLResponse
+                {
+                    Data = JsonConvert.DeserializeObject("{\"data\":{\"search\":{\"edges\":[{\"node\":{\"id\":\"MDQ6VXNlcjE0MTAxMDY=\",\"avatarUrl\":\"https://avatars2.githubusercontent.com/u/1410106?v=4\",\"name\":\"Shuvalov Anton\",\"login\":\"A\",\"bio\":\"Technical Group Manager at Lazada\",\"bioHTML\":\"<div>Technical Group Manager at Lazada</div>\",\"following\":{\"edges\":[{\"node\":{\"login\":\"robbyrussell\"}},{\"node\":{\"login\":\"tpope\"}},{\"node\":{\"login\":\"kangax\"}}]},\"email\":\"anton@shuvalov.info\",\"websiteUrl\":\"http://shuvalov.info\",\"repositories\":{\"edges\":[{\"node\":{\"id\":\"MDEwOlJlcG9zaXRvcnkxNzg2NDM4\",\"name\":\"move.js\",\"description\":\"CSS3 backed JavaScript animation framework\",\"stargazers\":{\"totalCount\":4341},\"viewerHasStarred\":false}},{\"node\":{\"id\":\"MDEwOlJlcG9zaXRvcnk5NTA1OTk5\",\"name\":\"grunt-ect\",\"description\":\"generates multiple html files from ect templates\",\"stargazers\":{\"totalCount\":8},\"viewerHasStarred\":false}},{\"node\":{\"id\":\"MDEwOlJlcG9zaXRvcnkxNDY2NzA0MQ==\",\"name\":\"simple-grid-stylus\",\"description\":\"Simple fixed customizable grid inspired bootstrap\",\"stargazers\":{\"totalCount\":0},\"viewerHasStarred\":false}},{\"node\":{\"id\":\"MDEwOlJlcG9zaXRvcnkxNDcxOTU1OQ==\",\"name\":\"shuvalov.info\",\"description\":\"My blog \",\"stargazers\":{\"totalCount\":1},\"viewerHasStarred\":false}}]}}}]}},\"errors\":null}")
+                }
+            };
+            var authControllerMock = new Mock<UserController>();
+            this.controller = new RepositoryController(configuration);
+            this.controller.Initialize(configuration, service);
+        }
+
+        [Fact]
+        public void GetTest()
+        {
+            var expected = JsonConvert.DeserializeObject("{\"data\":{\"search\":{\"edges\":[{\"node\":{\"id\":\"MDQ6VXNlcjE0MTAxMDY=\",\"avatarUrl\":\"https://avatars2.githubusercontent.com/u/1410106?v=4\",\"name\":\"Shuvalov Anton\",\"login\":\"A\",\"bio\":\"Technical Group Manager at Lazada\",\"bioHTML\":\"<div>Technical Group Manager at Lazada</div>\",\"following\":{\"edges\":[{\"node\":{\"login\":\"robbyrussell\"}},{\"node\":{\"login\":\"tpope\"}},{\"node\":{\"login\":\"kangax\"}}]},\"email\":\"anton@shuvalov.info\",\"websiteUrl\":\"http://shuvalov.info\",\"repositories\":{\"edges\":[{\"node\":{\"id\":\"MDEwOlJlcG9zaXRvcnkxNzg2NDM4\",\"name\":\"move.js\",\"description\":\"CSS3 backed JavaScript animation framework\",\"stargazers\":{\"totalCount\":4341},\"viewerHasStarred\":false}},{\"node\":{\"id\":\"MDEwOlJlcG9zaXRvcnk5NTA1OTk5\",\"name\":\"grunt-ect\",\"description\":\"generates multiple html files from ect templates\",\"stargazers\":{\"totalCount\":8},\"viewerHasStarred\":false}},{\"node\":{\"id\":\"MDEwOlJlcG9zaXRvcnkxNDY2NzA0MQ==\",\"name\":\"simple-grid-stylus\",\"description\":\"Simple fixed customizable grid inspired bootstrap\",\"stargazers\":{\"totalCount\":0},\"viewerHasStarred\":false}},{\"node\":{\"id\":\"MDEwOlJlcG9zaXRvcnkxNDcxOTU1OQ==\",\"name\":\"shuvalov.info\",\"description\":\"My blog \",\"stargazers\":{\"totalCount\":1},\"viewerHasStarred\":false}}]}}}]}},\"errors\":null}");
+            var result = (ObjectResult)controller.Get("tokentest", string.Empty);
+            var actual = (result.Value as GraphQLResponse).Data;
+
+            Assert.Equal(200, result.StatusCode.GetValueOrDefault(0));
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GetExceptionTest()
+        {
+            var result = (ObjectResult)controller.Get(string.Empty, string.Empty);
+
+            // assert status
+            Assert.Equal(500, result.StatusCode.GetValueOrDefault(0));
+
+            // assert specific type
+            Assert.IsType<Dictionary<string, ResponseException>>(result.Value);
+
+            // assert specfic value
+            var actual = (Dictionary<string, ResponseException>)result.Value;
+            Assert.NotNull(actual["error"]);
+        }
+
+    }
+}
---
+++ Api/Api/Controllers/RepositoryController.cs
@@ -0,0 +1,139 @@
+﻿using Api.Service;
+using GraphQL.Client;
+using GraphQL.Client.Exceptions;
+using GraphQL.Common.Request;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Net;
+
+namespace Api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/repository")]
+    public class RepositoryController : BaseController, IDisposable
+    {
+        /// <summary>
+        /// Configuration data from appsettings.json
+        /// </summary>
+        private IConfiguration configuration;
+
+        /// <summary>
+        /// GraphQL client to retrieve data from GitHub
+        /// </summary>
+        private GraphQLService service;
+
+        /// <summary>
+        /// App configuration with data from appsettings.json
+        /// </summary>
+        /// <param name="configuration">Configuration instance</param>
+        public RepositoryController(IConfiguration configuration)
+        {
+            this.Initialize(configuration, GraphQLService.GetInstance(configuration["GitHubEndpoints:GraphQL"]));
+        }
+
+        /// <summary>
+        /// Initializes the controller
+        /// </summary>
+        /// <param name="configuration">Configuration instance</param>
+        /// <param name="service">GraphQL service</param>
+        public void Initialize(IConfiguration configuration, GraphQLService service)
+        {
+            this.configuration = configuration;
+            this.service = service;
+        }
+
+        /// <summary>
+        /// Retrieves a user from GitHub based on search criteria
+        /// </summary>
+        /// <param name="token">Authorization token required from GitHub</param>
+        /// <param name="search">Search criteria (user name or nickname)</param>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Get(
+            [FromHeader(Name = "Authorization")] string token, 
+            [FromQuery] string search)
+        {
+            try
+            {
+                service.Headers.Add("Authorization", token);
+                service.Headers.Add("User-Agent", token);
+
+                GraphQLRequest request = new GraphQLRequest
+                {
+                    Query = @" 
+                        query { 
+                            search(query: ""#username#"", type: USER, first: 1) {
+                                edges {
+                                    node {
+                                        ... on User {
+                                            id,
+                                            avatarUrl,
+                                            name,
+                                            login,
+                                            bio,
+                                            bioHTML,
+                                            following (first: 3) {
+                                                edges {
+                                                    node {
+                                                        ... on User {
+                                                                login
+                                                            }
+                                                        }
+                                                    }
+                                                },
+                                                email,
+                                                websiteUrl,
+                                                repositories(first: 4) {
+                                                edges {
+                                                    node {
+                                                        id,
+                                                        name,
+                                                        description,
+                                                        stargazers {
+                                                            totalCount
+                                                        }
+                                                        viewerHasStarred
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }"
+                };
+                request.Query = request.Query.Replace("#username#", search);
+
+                var result = service.PostData(request).Result;
+                return StatusCode((int)HttpStatusCode.OK, result);
+        }
+            catch (AggregateException ex)
+            {
+                if (ex.InnerException is GraphQLHttpException)
+                {
+                    var message = (ex.InnerException as GraphQLHttpException).HttpResponseMessage;
+                    return StatusCode((int)message.StatusCode, message.ReasonPhrase);
+                }
+                return StatusCode((int)HttpStatusCode.InternalServerError, ParseException(ex));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, ParseException(ex));
+            }
+        }
+
+        /// <summary>
+        /// Disposing controller
+        /// </summary>
+        /// <param name="disposing">True if invoked by the Microsoft.AspNetCore.Mvc.Controller.Dispose method</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this.service.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
