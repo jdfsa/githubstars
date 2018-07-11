22b3f98071409268571fc074bca2a1aa353e9824
jacques.sa@outlook.com <jacques.sa@outlook.com>
2018-07-10 22:44:32 -0300

    complementing repository unit tests

--- Api/Api.Test/Controllers/RepositoryControllerTest.cs
+++ Api/Api.Test/Controllers/RepositoryControllerTest.cs
@@ -4,6 +4,7 @@ using Api.Test.Fakes;
 using Api.Test.Helper;
 using GraphQL.Common.Response;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Moq;
 using Newtonsoft.Json;
 using System.Collections.Generic;
@@ -13,11 +14,18 @@ namespace Api.Test.Controllers
 {
     public class RepositoryControllerTest
     {
-        RepositoryController controller;
+        private RepositoryController controller;
+        private IConfiguration configuration;
 
         public RepositoryControllerTest()
         {
-            var configuration = ConfigurationHelper.GetConfigurationMock();
+            this.configuration = ConfigurationHelper.GetConfigurationMock();
+            this.controller = new RepositoryController(configuration);
+        }
+
+        [Fact]
+        public void GetTest()
+        {
             var service = new GraphQLServiceFake("http://fakeendpoint.com/fake")
             {
                 Response = new GraphQLResponse
@@ -25,14 +33,8 @@ namespace Api.Test.Controllers
                     Data = JsonConvert.DeserializeObject("{\"search\":{\"edges\":[{\"node\":{\"id\":\"MDQ6VXNlcjE0MTAxMDY=\",\"avatarUrl\":\"https://avatars2.githubusercontent.com/u/1410106?v=4\",\"name\":\"Shuvalov Anton\",\"login\":\"A\",\"bio\":\"Technical Group Manager at Lazada\",\"bioHTML\":\"<div>Technical Group Manager at Lazada</div>\",\"following\":{\"edges\":[{\"node\":{\"login\":\"robbyrussell\"}},{\"node\":{\"login\":\"tpope\"}},{\"node\":{\"login\":\"kangax\"}}]},\"email\":\"anton@shuvalov.info\",\"websiteUrl\":\"http://shuvalov.info\",\"repositories\":{\"edges\":[{\"node\":{\"id\":\"MDEwOlJlcG9zaXRvcnkxNzg2NDM4\",\"name\":\"move.js\",\"description\":\"CSS3 backed JavaScript animation framework\",\"stargazers\":{\"totalCount\":4341},\"viewerHasStarred\":false}},{\"node\":{\"id\":\"MDEwOlJlcG9zaXRvcnk5NTA1OTk5\",\"name\":\"grunt-ect\",\"description\":\"generates multiple html files from ect templates\",\"stargazers\":{\"totalCount\":8},\"viewerHasStarred\":false}},{\"node\":{\"id\":\"MDEwOlJlcG9zaXRvcnkxNDY2NzA0MQ==\",\"name\":\"simple-grid-stylus\",\"description\":\"Simple fixed customizable grid inspired bootstrap\",\"stargazers\":{\"totalCount\":0},\"viewerHasStarred\":false}},{\"node\":{\"id\":\"MDEwOlJlcG9zaXRvcnkxNDcxOTU1OQ==\",\"name\":\"shuvalov.info\",\"description\":\"My blog \",\"stargazers\":{\"totalCount\":1},\"viewerHasStarred\":false}}]}}}]}}")
                 }
             };
-            var authControllerMock = new Mock<UserController>();
-            this.controller = new RepositoryController(configuration);
             this.controller.Initialize(configuration, service);
-        }
 
-        [Fact]
-        public void GetTest()
-        {
             var expected = new UserInfo
             {
                 Id = "MDQ6VXNlcjE0MTAxMDY=",
@@ -82,8 +84,17 @@ namespace Api.Test.Controllers
         }
 
         [Fact]
-        public void GetExceptionTest()
+        public void GetExceptionNoTokenTest()
         {
+            var service = new GraphQLServiceFake("http://fakeendpoint.com/fake")
+            {
+                Response = new GraphQLResponse
+                {
+                    Data = JsonConvert.DeserializeObject("{\"search\":{\"edges\":[{\"node\":{\"id\":\"MDQ6VXNlcjE0MTAxMDY=\",\"avatarUrl\":\"https://avatars2.githubusercontent.com/u/1410106?v=4\",\"name\":\"Shuvalov Anton\",\"login\":\"A\",\"bio\":\"Technical Group Manager at Lazada\",\"bioHTML\":\"<div>Technical Group Manager at Lazada</div>\",\"following\":{\"edges\":[{\"node\":{\"login\":\"robbyrussell\"}},{\"node\":{\"login\":\"tpope\"}},{\"node\":{\"login\":\"kangax\"}}]},\"email\":\"anton@shuvalov.info\",\"websiteUrl\":\"http://shuvalov.info\",\"repositories\":{\"edges\":[{\"node\":{\"id\":\"MDEwOlJlcG9zaXRvcnkxNzg2NDM4\",\"name\":\"move.js\",\"description\":\"CSS3 backed JavaScript animation framework\",\"stargazers\":{\"totalCount\":4341},\"viewerHasStarred\":false}},{\"node\":{\"id\":\"MDEwOlJlcG9zaXRvcnk5NTA1OTk5\",\"name\":\"grunt-ect\",\"description\":\"generates multiple html files from ect templates\",\"stargazers\":{\"totalCount\":8},\"viewerHasStarred\":false}},{\"node\":{\"id\":\"MDEwOlJlcG9zaXRvcnkxNDY2NzA0MQ==\",\"name\":\"simple-grid-stylus\",\"description\":\"Simple fixed customizable grid inspired bootstrap\",\"stargazers\":{\"totalCount\":0},\"viewerHasStarred\":false}},{\"node\":{\"id\":\"MDEwOlJlcG9zaXRvcnkxNDcxOTU1OQ==\",\"name\":\"shuvalov.info\",\"description\":\"My blog \",\"stargazers\":{\"totalCount\":1},\"viewerHasStarred\":false}}]}}}]}}")
+                }
+            };
+            this.controller.Initialize(configuration, service);
+
             var result = (ObjectResult)controller.Get(string.Empty, string.Empty);
 
             // assert status
@@ -97,5 +108,55 @@ namespace Api.Test.Controllers
             Assert.NotNull(actual["errors"]);
         }
 
+        [Fact]
+        public void StarringTest()
+        {
+            var expected = JsonConvert.DeserializeObject("{\"addStar\":{\"clientMutationId\":\"client_id_test\",\"starrable\":{\"id\":\"repo_id_test\",\"viewerHasStarred\":true}}}");
+
+            var service = new GraphQLServiceFake("http://fakeendpoint.com/fake")
+            {
+                Response = new GraphQLResponse
+                {
+                    Data = expected
+                }
+            };
+            this.controller.Initialize(this.configuration, service);
+
+            var result = (ObjectResult)controller.Starring("tokentest", "client_id_test", new StarringRequest
+            {
+                RepositoryId = "repo_id_test",
+                Starring = true
+            });
+
+            Assert.Equal(200, result.StatusCode.GetValueOrDefault(0));
+            Assert.True(expected.EquivalentTo(result.Value));
+        }
+
+        [Fact]
+        public void StarringExceptionNoTokenTest()
+        {
+            var expected = JsonConvert.DeserializeObject("{\"addStar\":{\"clientMutationId\":\"client_id_test\",\"starrable\":{\"id\":\"repo_id_test\",\"viewerHasStarred\":true}}}");
+
+            var service = new GraphQLServiceFake("http://fakeendpoint.com/fake")
+            {
+                Response = new GraphQLResponse
+                {
+                    Data = expected
+                }
+            };
+            this.controller.Initialize(this.configuration, service);
+
+            var result = (ObjectResult)controller.Starring(string.Empty, string.Empty, new StarringRequest());
+
+            // assert status
+            Assert.Equal(500, result.StatusCode.GetValueOrDefault(0));
+
+            // assert specific type
+            Assert.IsType<Dictionary<string, List<string>>>(result.Value);
+
+            // assert specfic value
+            var actual = result.Value as Dictionary<string, List<string>>;
+            Assert.NotNull(actual["errors"]);
+        }
     }
 }
