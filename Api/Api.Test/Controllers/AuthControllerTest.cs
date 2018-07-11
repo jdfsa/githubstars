0cc2849f9a6be8f21cae1ecd161afa86ea4eaaaa
jacques.sa@outlook.com <jacques.sa@outlook.com>
2018-07-10 22:30:57 -0300

    fixing authcontroller tests

--- Api/Api.Test/Controllers/AuthControllerTest.cs
+++ Api/Api.Test/Controllers/AuthControllerTest.cs
@@ -1,7 +1,10 @@
 using Api.Controllers;
+using Api.Model;
 using Api.Test.Fakes;
 using Api.Test.Helper;
+using GraphQL.Common.Response;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Moq;
 using Newtonsoft.Json;
 using System;
@@ -13,26 +16,22 @@ namespace Api.Test.Controllers
 {
     public class AuthControllerTest
     {
-        AuthController controller;
+        private IConfiguration configuration;
+        private AuthController controller;
 
         public AuthControllerTest()
         {
-            var configuration = ConfigurationHelper.GetConfigurationMock();
-
-            var random = new Mock<Random>();
-            random.Setup(x => x.Next()).Returns(99887766);
-
-            var httpClient = new HttpClient(new HttpHandlerFake(
-                HttpStatusCode.OK,
-                "{\"access_token\":\"access_token_test\",\"token_type\":\"bearer\",\"scope\":\"scope_test\"}"));
-
+            this.configuration = ConfigurationHelper.GetConfigurationMock();
             this.controller = new AuthController(configuration);
-            this.controller.Initialize(configuration, random.Object, httpClient);
         }
 
         [Fact]
         public void TestGetUrl()
         {
+            var randomMock = new Mock<Random>();
+            randomMock.Setup(x => x.Next()).Returns(99887766);
+
+            this.controller.Initialize(configuration, randomMock.Object, null, null);
             var expectedUrl = "http://url-test.com/authorize?client_id=_client_id_test&scope=_scope:t1,scope:t2&redirect_uri=http://url-callback-teste.com&state=R2l0SHViU3RhcnM5OTg4Nzc2Ng==";
             var actualUrl = controller.GetUrl("http://url-callback-teste.com");
 
@@ -42,11 +41,35 @@ namespace Api.Test.Controllers
         [Fact]
         public void TestGetToken()
         {
+            var httpClient = new HttpClient(new HttpHandlerFake(
+                HttpStatusCode.OK,
+                "{\"access_token\":\"access_token_test\",\"token_type\":\"bearer\",\"scope\":\"scope_test\"}"));
+
+            var graphService = new GraphQLServiceFake("http://fakeendpoint.com/fake")
+            {
+                Response = new GraphQLResponse
+                {
+                    Data = JsonConvert.DeserializeObject(JsonConvert.SerializeObject(new
+                    {
+                        viewer = new { ID = "idtest" }
+                    }))
+                }
+            };
+
+            this.controller.Initialize(configuration, null, httpClient, graphService);
+
             var result = (ObjectResult)controller.GetToken("githubcodetest", "statetest");
-            var expected = JsonConvert.DeserializeObject("{\"access_token\":\"access_token_test\",\"token_type\":\"bearer\",\"scope\":\"scope_test\"}");
+            var expected = new AccessTokenResponse
+            {
+                AccessToken = "eyJhbGciOiJub25lIiwidHlwIjoiSldUIn0.eyJHaXRIdWJUb2tlbiI6ImJlYXJlciBhY2Nlc3NfdG9rZW5fdGVzdCIsIlVzZXIiOnsiaWQiOm51bGwsImF2YXRhclVybCI6bnVsbCwibmFtZSI6bnVsbCwibG9naW4iOm51bGx9fQ",
+                TokenType = "bearer",
+                Scope = "scope_test",
+                Error = null,
+                ErrorDescription = null
+            };
 
             Assert.Equal((int)HttpStatusCode.OK, result.StatusCode);
-            Assert.Equal(expected, result.Value);
+            Assert.True(expected.EquivalentTo(result.Value));
         }
     }
 }
